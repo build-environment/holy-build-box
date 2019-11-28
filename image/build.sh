7b44db82bc6d100020f2893b943071cb4a4cb614
Ryan Mast <nightlark@users.noreply.github.com>
2019-11-28 11:05:54 -0800

    Add a copy of git 2.24.0

--- image/build.sh
+++ image/build.sh
@@ -14,6 +14,7 @@ GCC_LIBSTDCXX_VERSION=8.3.0
 ZLIB_VERSION=1.2.11
 OPENSSL_VERSION=1.0.2q
 CURL_VERSION=7.63.0
+GIT_VERSION=2.24.0
 SQLITE_VERSION=3260000
 SQLITE_YEAR=2018
 CENTOS_VERSION=$(tr -d 'a-zA-Z() \n' < /etc/centos-release)
@@ -36,6 +37,7 @@ SKIP_PKG_CONFIG=${SKIP_PKG_CONFIG:-$SKIP_TOOLS}
 SKIP_CCACHE=${SKIP_CCACHE:-$SKIP_TOOLS}
 SKIP_CMAKE=${SKIP_CMAKE:-$SKIP_TOOLS}
 SKIP_PYTHON=${SKIP_PYTHON:-$SKIP_TOOLS}
+SKIP_GIT=${SKIP_GIT:-$SKIP_TOOLS}
 
 SKIP_LIBSTDCXX=${SKIP_LIBSTDCXX:-$SKIP_LIBS}
 SKIP_ZLIB=${SKIP_ZLIB:-$SKIP_LIBS}
@@ -289,6 +291,30 @@ if ! eval_bool "$SKIP_CMAKE"; then
 		run make -j$MAKE_CONCURRENCY
 		run make install
 		run strip --strip-all /hbb/bin/cmake
+	)
+	if [[ "$?" != 0 ]]; then false; fi
+
+	echo "Leaving source directory"
+	popd >/dev/null
+	run rm -rf cmake-$CMAKE_VERSION
+fi
+
+
+### Git
+
+if ! eval_bool "$SKIP_GIT"; then
+	header "Installing Git $GIT_VERSION"
+	download_and_extract git-$GIT_VERSION.tar.gz \
+		git-$GIT_VERSION \
+		https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz
+
+	(
+		activate_holy_build_box_deps_installation_environment
+		run make configure
+		run ./configure --prefix=/hbb
+		run make -j$MAKE_CONCURRENCY
+		run make install
+		run strip --strip-all /hbb/bin/git
 	)
 	if [[ "$?" != 0 ]]; then false; fi
 
